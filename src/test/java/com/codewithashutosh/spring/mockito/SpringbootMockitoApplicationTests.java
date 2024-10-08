07fbb754456479ceff96cd71fc72944415acc20f
AshutoshG1 <ashutoshgautamspn@gmail.com>
2024-10-08 23:12:37 +0530

    added comment

--- src/test/java/com/codewithashutosh/spring/mockito/SpringbootMockitoApplicationTests.java
+++ src/test/java/com/codewithashutosh/spring/mockito/SpringbootMockitoApplicationTests.java
@@ -20,6 +20,8 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 public class SpringbootMockitoApplicationTests {
 
+
+
 	@Autowired
 	private UserService service;
 
@@ -54,4 +56,46 @@ public class SpringbootMockitoApplicationTests {
 		service.deleteUser(user);
 		verify(repository, times(1)).delete(user);
 	}
+
+	/*
+	@ExtendWith(SpringExtension.class):
+
+This annotation is part of JUnit 5 and is
+used to integrate the Spring TestContext
+Framework into JUnit 5 tests.
+SpringExtension allows the use
+of Spring's features (like dependency injection)
+ in your unit tests. Essentially, it makes
+ sure that Spring's context is loaded for the test.
+
+@SpringBootTest:
+
+This annotation is used to tell Spring Boot
+ to look for a SpringBootApplication or
+  @Configuration class and use it to load
+  the entire Spring application context.
+  It is typically used for integration
+  tests where you want to load the full
+  application context and test how different
+  components interact.
+
+@MockBean:
+
+This annotation is used to create and
+inject a mock of a Spring component
+(like a repository or service).
+It is provided by Spring Boot and used
+in tests to mock dependencies, ensuring that
+ actual beans are not invoked during unit tests.
+
+verify(repository, times(1)).delete(user);:
+
+This statement is from Mockito,
+a popular mocking framework in Java.
+ It verifies that the delete method of the
+ repository is called exactly once with
+  the user object as a parameter. verify
+   ensures that the correct interactions
+   happen with the mock objects.
+	 */
 }
